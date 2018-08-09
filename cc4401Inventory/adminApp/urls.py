14919466aeef53a561c49bc8f02c68a5aeeb41c4
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-08-09 18:45:20 -0400

    Agregadas urls prestamos

--- cc4401Inventory/adminApp/urls.py
+++ cc4401Inventory/adminApp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('user-panel/', views.user_panel, name="user-panel"),
     path('items-panel/', views.items_panel, name="items-panel"),
     path('actions-panel/', views.actions_panel, name="actions-panel"),
-    path('actions-panel/modify', views.modify_reservations, name="modify_reservations")
-
+    path('actions-panel/modify', views.modify_reservations, name="modify_reservations"),
+    path('actions-panel/receive-loans', views.modify_loans, name="receive_loans"),
+    path('actions-panel/lost-loans', views.modify_loans, name="lost_loans")
 ]
