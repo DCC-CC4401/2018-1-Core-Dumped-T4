07c75f4c7a1a9f3cb1af3aef9b10c75083327ebe
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-08-09 19:04:08 -0400

    Cambio estado modelo prestamo.
    Ahora el estado no esta en el modelo abstracto.

--- cc4401Inventory/loansApp/models.py
+++ cc4401Inventory/loansApp/models.py
@@ -4,4 +4,21 @@ from django.db import models
 
 
 class Loan(Action):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    VIGENTE = 'V'
+    CADUCADO = 'C'
+    PERDIDO = 'L'
+    STATES = (
+        (VIGENTE, 'Vigente'),
+        (CADUCADO, 'Caducado'),
+        (PERDIDO, 'Perdido')
+    )
+    state = models.CharField(
+        'Estado',
+        choices=STATES,
+        max_length=1,
+        default=VIGENTE
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE
+    )
