bdc6e0bc414085ed0257661ac843484ac9182c15
danno-s <danielsoto.3004@gmail.com>
2018-08-21 13:31:42 -0300

    arregla error en reverse de espacios

--- cc4401Inventory/mainApp/urls.py
+++ cc4401Inventory/mainApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('articles/', views.landing_articles, name='landing_articles'),
     path('spaces/', views.landing_spaces, name='landing_spaces'),
-    path('spaces/<str:date>/<str:filter>', views.landing_spaces, name='landing_spaces'),
+    path('spaces/<str:date>/<str:filter>', views.landing_spaces, name='landing_spaces_filter'),
     path('search/', views.search, name='search'),
     path('create_reservation/', views.crear_reserva1, name='create_reservation'),
 ]
